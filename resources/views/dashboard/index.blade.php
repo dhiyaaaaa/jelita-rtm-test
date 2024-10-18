70445c74c1403bb20fb670fa8c849c85e0d7e88a
Ahita Bisma Adlula <ahita.adlula@mhs.unsoed.ac.id>
2024-10-18 07:54:14 +0700

    perbaiki url user manual

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -50,7 +50,8 @@
 
                             {{-- User Manual --}}
                             <div class="">
-                                <a href="{{ asset('user_manual/Auditor.pdf') }}" target="_blank" class="btn btn-outline-info">
+                                <a href="{{ asset('user_manual/Auditor.pdf') }}" target="_blank"
+                                    class="btn btn-outline-info">
                                     <i class="fa fa-book mr-2"></i> User Manual
                                 </a>
                             </div>
@@ -129,7 +130,8 @@
                     <div class="card-body">
                         {{-- User Manual --}}
                         <div class="mb-3">
-                            <a href="{{ asset('user_manual/Auditan.pdf') }}" target="_blank" class="btn btn-outline-info">
+                            <a href="{{ asset('user_manual/Auditan.pdf') }}" target="_blank"
+                                class="btn btn-outline-info">
                                 <i class="fa fa-book mr-2"></i> User Manual
                             </a>
                         </div>
