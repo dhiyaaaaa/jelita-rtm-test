c122f7d4d6174ef009db6f7ed1cab3d3cab0b9d5
Ahita Bisma Adlula <ahita.adlula@mhs.unsoed.ac.id>
2024-10-23 12:02:50 +0700

    perbaiki tampilan hasil audit

--- resources/views/admin/audit/hasil_audit/show.blade.php
+++ resources/views/admin/audit/hasil_audit/show.blade.php
@@ -22,10 +22,10 @@
                             <tr>
                                 <th class="text-center">No</th>
                                 <th class="text-center">Prodi</th>
-                                {{-- <th class="text-center">Auditan</th>
+                                <th class="text-center">Auditan</th>
                                 <th class="text-center">Auditor</th>
                                 <th class="text-center">Status Audit Dokumen</th>
-                                <th class="text-center">Status Audit Lapangan</th> --}}
+                                <th class="text-center">Status Audit Lapangan</th>
                                 <th class="text-center">Aksi</th>
                             </tr>
                         </thead>
@@ -41,7 +41,7 @@
                                     <td class="align-middle">{{ $item->nama }} {{ $item->jenjang->nama }}</td>
 
                                     {{-- Auditee --}}
-                                    {{-- <td class="align-middle">
+                                    <td class="align-middle">
                                         @if ($item->auditee->isNotEmpty())
                                             @foreach ($item->auditee as $auditee)
                                                 <p style="margin:0; padding:0;">{{ $loop->iteration }}.
@@ -51,10 +51,10 @@
                                         @else
                                             -
                                         @endif
-                                    </td> --}}
+                                    </td>
 
                                     {{-- Auditor --}}
-                                    {{-- <td class="align-middle">
+                                    <td class="align-middle">
                                         @if ($item->auditee->isNotEmpty())
                                             @php
                                                 $uniqueAuditors = collect();
@@ -83,10 +83,10 @@
                                         @else
                                             <a class="btn disabled text-center">Belum ada Auditor</a>
                                         @endif
-                                    </td> --}}
+                                    </td>
 
                                     {{-- Status Audit Dokumen --}}
-                                    {{-- <td class="text-center align-middle">
+                                    <td class="text-center align-middle">
                                         @php
                                             $status_audit_auditee = $item->status_audit_auditee->first();
                                             $status_audit_auditor = $item->status_audit_auditor->first();
@@ -108,10 +108,10 @@
                                         @else
                                             <a class="btn disabled">Belum Mulai</a>
                                         @endif
-                                    </td> --}}
+                                    </td>
 
                                     {{-- Status Audit Lapangan --}}
-                                    {{-- <td class="text-center align-middle">
+                                    <td class="text-center align-middle">
                                         @php
                                             $berita_acara = $item->berita_acara->first();
                                             $ptk = $item->ptk->first();
@@ -181,7 +181,7 @@
                                         @else
                                             <a class="btn disabled">Belum Ada</a>
                                         @endif
-                                    </td> --}}
+                                    </td>
 
                                     {{-- Aksi --}}
                                     <td class="text-center align-middle">
@@ -308,10 +308,10 @@
                             <tr>
                                 <th class="text-center">No</th>
                                 <th class="text-center">Fakultas/Unit</th>
-                                {{-- <th class="text-center">Auditan</th>
+                                <th class="text-center">Auditan</th>
                                 <th class="text-center">Auditor</th>
                                 <th class="text-center">Status Audit Dokumen</th>
-                                <th class="text-center">Status Audit Lapangan</th> --}}
+                                <th class="text-center">Status Audit Lapangan</th>
                                 <th class="text-center">Aksi</th>
                             </tr>
                         </thead>
@@ -329,7 +329,7 @@
                                     </td>
 
                                     {{-- Auditee --}}
-                                    {{-- <td class="align-middle">
+                                    <td class="align-middle">
                                         @if ($item->auditee->isNotEmpty())
                                             @foreach ($item->auditee as $auditee)
                                                 <p style="margin:0; padding:0;">{{ $loop->iteration }}.
@@ -339,10 +339,10 @@
                                         @else
                                             -
                                         @endif
-                                    </td> --}}
+                                    </td>
 
                                     {{-- Auditor --}}
-                                    {{-- <td class="align-middle">
+                                    <td class="align-middle">
                                         @if ($item->auditee->isNotEmpty())
                                             @php
                                                 $uniqueAuditors = collect();
@@ -371,10 +371,10 @@
                                         @else
                                             <a class="btn disabled text-center">Belum ada Auditor</a>
                                         @endif
-                                    </td> --}}
+                                    </td>
 
                                     {{-- Status Audit Dokumen --}}
-                                    {{-- <td class="text-center align-middle">
+                                    <td class="text-center align-middle">
                                         @php
                                             $status_audit_auditee = $item->status_audit_auditee->first();
                                             $status_audit_auditor = $item->status_audit_auditor->first();
@@ -397,10 +397,10 @@
                                         @else
                                             <a class="btn disabled">Belum Mulai</a>
                                         @endif
-                                    </td> --}}
+                                    </td>
 
                                     {{-- Status Audit Lapangan --}}
-                                    {{-- <td class="text-center align-middle">
+                                    <td class="text-center align-middle">
                                         @php
                                             $berita_acara = $item->berita_acara->first();
                                             $ptk = $item->ptk->first();
@@ -470,7 +470,7 @@
                                         @else
                                             <a class="btn disabled">Belum Ada</a>
                                         @endif
-                                    </td> --}}
+                                    </td>
 
                                     {{-- Aksi --}}
                                     <td class="text-center align-middle">
