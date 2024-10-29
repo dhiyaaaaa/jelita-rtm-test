ab7c377c8043b0db93b413cee898bd6d1f0c4335
Ahita Bisma Adlula <ahita.adlula@mhs.unsoed.ac.id>
2024-10-29 08:18:46 +0700

    remove debugging

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -40,9 +40,9 @@
                     Untuk sementara, Anda dapat <a href="{{ route('dashboard') }}">kembali ke dashboard.</a>
                 </p>
 
-                <pre style="background: #f8f8f8; padding: 10px; border: 1px solid #ccc;">
+                {{-- <pre style="background: #f8f8f8; padding: 10px; border: 1px solid #ccc;">
                     {{ file_get_contents(storage_path('logs/laravel.log')) }}
-                </pre>
+                </pre> --}}
             </div>
         </div>
         <!-- /.error-page -->
