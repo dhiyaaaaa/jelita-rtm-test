33402c89c7b69c9cfc629a38270cf9cad3797c7a
Ahita Bisma Adlula <ahita.adlula@mhs.unsoed.ac.id>
2024-10-31 21:57:57 +0700

    perbaiki tampilan login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,93 +5,100 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login | JELITA</title>
-    <link rel="icon" href="{{ asset('dist/img/logo_jelita.jpg') }}" />
+    <link rel="icon" href="{{ asset('dist/img/logo_jelita.png') }}" />
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- SweetAlert2 -->
+    <link rel="stylesheet" href="{{ asset('plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css') }}">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+    <!-- Login Style -->
+    <link rel="stylesheet" href="{{ asset('dist/css/login.css') }}">
 </head>
 
-<body class="hold-transition login-page">
-    <div class="login-box">
-        <!-- /.login-logo -->
-        <div class="card card-outline card-primary">
-            <div class="card-header text-center">
-                <a href="" class="h1"><b>JELITA</b></a>
+<body>
+    {{-- Sweet Alert --}}
+    @include('sweetalert::alert')
+
+    <div id="container" class="min-vh-100" style="background-image: url('{{ asset('dist/img/bg.jpg') }}');">
+
+        <!-- kiri -->
+        <div id="kiri" class="col-8 h-100">
+            {{-- Logo Unsoed --}}
+            <div class="d-flex align-items-center fade-in no-select">
+                <img src="{{ asset('dist/img/logo_unsoed.png') }}" alt="UNSOED" id="logo-unsoed">
+                <p class="text-bold text-light" style="font-size: 3vw; margin-left:1.5vw;">Universitas Jenderal
+                    Soedirman</p>
             </div>
-            <div class="card-body">
-                <p class="login-box-msg">Sign in to start your session</p>
 
-                <form action="{{ route('login.store') }}" method="post" id="login-form">
-                    @csrf
-                    @if (session('error'))
-                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                            {{ session('error') }}
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                    @endif
-                    <div class="input-group mt-3">
-                        <input type="email" class="form-control @error('email') is-invalid @enderror"
-                            placeholder="Email" name="email">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
-                            </div>
-                        </div>
-                    </div>
+            {{-- Typing JELITA --}}
+            <div style="margin-top:3vw;">
+                <p class="typing no-select">Jenderal Soedirman Internal</p>
+                <p class="typing no-select">Quality Assurance</p>
+            </div>
+
+            {{-- Animasi --}}
+            <div class="d-flex justify-content-end fade-in">
+                <img src="{{ asset('dist/img/animasi_1.png') }}" alt="Animasi" class="shake" id="animasi-1">
+            </div>
+        </div>
+
+        <!-- kanan -->
+        <div id="kanan">
+
+            {{-- Logo JELITA --}}
+            <img src="{{ asset('dist/img/logo_jelita.png') }}" alt="Jelita" class="no-select" id="logo-jelita">
+
+            <!-- Form Login -->
+            <form action="{{ route('login.store') }}" method="post" id="login-form" class="w-100 px-4">
+                @csrf
+                <div class="div-wrapper w-100">
+                    <input id="inputEmail" type="email" autocomplete="off" placeholder=" " required
+                        class="input w-100" name="email">
+                    <label for="inputEmail" class="label" style="font-weight: 400;">Email</label>
                     @error('email')
-                        <span class="text-danger d-block" style="font-size: 14px">{{ $message }}</span>
+                        <span class="text-danger d-block" style="font-size: 16px">{{ $message }}</span>
                     @enderror
+                </div>
 
-                    <div class="input-group mt-3">
-                        <input type="password" class="form-control @error('password') is-invalid @enderror"
-                            placeholder="Password" name="password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
-                            </div>
-                        </div>
-                    </div>
+                <div class="div-wrapper w-100 mt-4">
+                    <input id="inputPassword" type="password" autocomplete="off" placeholder=" " required
+                        class="input w-100" name="password">
+                    <label for="inputPassword" class="label" style="font-weight: 400;">Password</label>
                     @error('password')
-                        <span class="text-danger d-block"
-                            style="font-size: 14px">{{ $message }}</span>
+                        <span class="text-danger d-block" style="font-size: 16px">{{ $message }}</span>
                     @enderror
+                </div>
 
-                    <div class="row mt-3">
-                        <!-- /.col -->
-                        <div class="col">
-                            <button type="submit" id="login-button" class="btn btn-primary btn-block">Sign In</button>
-                            <button id="login-button-loading" class="btn btn-primary btn-block d-none" type="button"
-                                disabled>
-                                <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-                                Loading...
-                            </button>
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                </form>
+                <div class="div-wrapper">
+                    <button type="submit" id="login-button"
+                        class="btn btn-primary btn-block mt-4 shadow-sm signin-btn">Sign In</button>
+                    <button id="login-button-loading" class="btn btn-primary btn-block mt-5 shadow-sm d-none"
+                        type="button" disabled>
+                        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                        Loading...
+                    </button>
+                </div>
+            </form>
 
-                {{-- Button Sign In SSO --}}
-                <div class="row mt-3">
-                    <!-- /.col -->
-                    <div class="col">
-                        <a href="{{ route('login.google') }}" class="btn btn-light btn-block border-dark">
-                            <img src="{{ asset('dist/img/logo_unsoed.png') }}" alt="Unsoed"
-                                style="width: 20px; height:20px">
-                            SSO/Kori Unsoed</a>
-                    </div>
-                    <!-- /.col -->
+            {{-- SSO --}}
+            <div class="w-100 px-4 mt-4 mb-5">
+                <div class="div-wrapper">
+                    <a href="{{ route('login.google') }}"
+                        class="btn btn-light border-dark btn-block shadow-sm sso-btn">
+                        <img src="{{ asset('dist/img/logo_unsoed.png') }}" alt="Unsoed"
+                            style="width: 20px; height:20px">
+                        Sign In dengan SSO</a>
                 </div>
             </div>
-            <!-- /.card-body -->
         </div>
-        <!-- /.card -->
+
     </div>
+
+
     <!-- /.login-box -->
 
     <!-- jQuery -->
